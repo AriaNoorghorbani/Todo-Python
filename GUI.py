4ba9e23cec7a69510709ef6bc70377a98e8af800
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-11 10:33:55 +0300

    Complete App:)

--- GUI.py
+++ GUI.py
@@ -7,10 +7,10 @@ sg.theme('black')
 clock = sg.Text(time.strftime("%b %d, %Y %H:%M:%S"), key="time")
 label = sg.Text('Type one To-Do')
 input_box = sg.InputText(tooltip='Enter a To-Do', key="todo")
-add_button = sg.Button('Add', size="40")
+add_button = sg.Button(size=10, image_source="images/add.png", mouseover_colors="LightBlue2", key="Add")
 todo_list = sg.Listbox(values=get_todos(), key="todos", enable_events=True, size=[40, 10])
 edit_button = sg.Button('Edit')
-complete_button = sg.Button('Complete')
+complete_button = sg.Button(key='Complete', image_source="images/complete.png", mouseover_colors="LightBlue2")
 exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
