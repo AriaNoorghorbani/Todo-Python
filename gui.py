fa4aa42a0a98d41fe851fa5be8eefe0303d08eb2
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-11 12:44:31 +0300

    Fix bugs

--- gui.py
+++ gui.py
@@ -14,10 +14,12 @@ sg.theme('black')
 clock = sg.Text(time.strftime("%b %d, %Y %H:%M:%S"), key="time")
 label = sg.Text('Type one To-Do')
 input_box = sg.InputText(tooltip='Enter a To-Do', key="todo")
-add_button = sg.Button(size=10, image_source="images/add.png", mouseover_colors="LightBlue2", key="Add")
+# add_button = sg.Button(size=10, image_source="add.png", mouseover_colors="LightBlue2", key="Add")
+add_button = sg.Button("Add",size=10, mouseover_colors="LightBlue2")
 todo_list = sg.Listbox(values=get_todos(), key="todos", enable_events=True, size=[40, 10])
 edit_button = sg.Button('Edit')
-complete_button = sg.Button(key='Complete', image_source="images/complete.png", mouseover_colors="LightBlue2")
+# complete_button = sg.Button(key='Complete', image_source="complete.png", mouseover_colors="LightBlue2")
+complete_button = sg.Button("Complete")
 exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
@@ -51,14 +53,16 @@ while True:
                 todo = value['todos'][0]
                 todos = get_todos()
                 todo_to_edit = todos.index(todo)
-                todos[todo_to_edit] = value['todo'] + "\n"
+                todos[todo_to_edit] = value['todo']
                 write_file(todos)
                 window['todos'].update(todos)
             except IndexError:
                 sg.popup("Please chose a todo", font=["Helvetica", "17"])
                 continue
+            except ValueError:
+                continue
         case "todos":
-            fill_input =value['todos'][0].strip()
+            fill_input =value['todos'][0]
             window['todo'].update(value=fill_input)
         case "Complete":
             try:
